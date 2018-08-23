1a7efa19854720b9e419ea6a593003aa1c93f74c
Brian Cilenti <briancilenti@gmail.com>
2018-08-22 23:19:31 -0700

    test

--- flask_heroku_example/app.py
+++ flask_heroku_example/app.py
@@ -4,21 +4,22 @@ from flask import Flask, render_template, g
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
-
-
-def connect_db():
-    return psycopg2.connect(os.environ.get('DATABASE_URL'))
-
-
-# @app.before_request
-# def before_request():
-#     g.db_conn = connect_db()
 
 
 @app.route('/')
 def index():
-    # cur = g.db_conn.cursor()
-    # cur.execute("SELECT * FROM country;")
-    # return render_template('index.html', countries=cur.fetchall())
     return "hello flask - this is a change"
+
+# cur = g.db_conn.cursor()
+# cur.execute("SELECT * FROM country;")
+# return render_template('index.html', countries=cur.fetchall())
+
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'XYZ')
+
+#
+# def connect_db():
+#     return psycopg2.connect(os.environ.get('DATABASE_URL'))
+
+# @app.before_request
+# def before_request():
+#     g.db_conn = connect_db()
